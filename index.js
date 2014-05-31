76a655e9ac710c44f0e44f91e37bb5c72aac08fe
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-05-31 13:21:58 +0200

    implement cli call to package

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict"
 
 var fs = require('fs');
 
 module.exports = function (name, cb) {
+	console.log('yyy');
 	fs.readFile('/Users/alessandrominoccheri/Sites/klikkahotel.com/css/backend/_style.css', 'utf8', function (err,data) {
 		if (err) {
 		    return console.log(err);
