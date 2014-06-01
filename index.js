cbadf301b7788e31329b3b75c341cee35ae3d199
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-06-01 12:17:15 +0200

    create regular expression to retrieve a class inside a css file

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 var fs = require('fs');
 
 module.exports = function (name, cb) {
-	console.log('yyy');
 	fs.readFile('/Users/alessandrominoccheri/Sites/klikkahotel.com/css/backend/_style.css', 'utf8', function (err,data) {
 		if (err) {
 		    return console.log(err);
 		}
 
-		console.log(data);
+		var class_to_find = 'label-blu';
+		//var found = css.match(/([#|\.]?)([\w|:|\s|\.]+)/gmi).length;*/
+		var found = data.match(/\.label-blu([,:\s\.][^\{]*)?\{/gmi).length;
+		console.log('found: ' + found);
 	});
 };
