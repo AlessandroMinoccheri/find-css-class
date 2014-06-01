2f888bde9f71abf3b30efd55ed8de43439e6cc17
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-06-01 23:50:01 +0200

    added search in a directory all css file and parse it

--- cli.js
+++ cli.js
@@ -3,38 +3,6 @@
 var pkg = require('./package.json');
 var find = require('./index');
 
-function help() {
-	console.log(pkg.description);
-	console.log('');
-	console.log('Usage');
-	console.log('  $ package-info <package-name>');
-	console.log('');
-	console.log('Example');
-	console.log('  $ package-info pageres');
-}
-
-/*if (!input || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}*/
-
-/*process.argv.forEach(function (val, index, array) {
-	find(input(val), function (err, version) {
-		if (err) {
-			console.error(err);
-			process.exit(1);
-			return;
-		}
-
-		console.log(version);
-	});
-});*/
-
 var args = process.argv[2];
 if((args != null) && (args != '') && (args != undefined)){
 	find(args, function (err, version) {
@@ -49,5 +17,3 @@ if((args != null) && (args != '') && (args != undefined)){
 }
 
 return;
-
-
--- index.js
+++ index.js
@@ -7,22 +7,55 @@ RegExp.quote = function(str) {
 };
 
 module.exports = function (name, cb) {
-	console.log(name);
 	// /Users/alessandrominoccheri/Sites/klikkahotel.com/css/backend/_style.css
-	fs.readFile(name, 'utf8', function (err,data) {
-		if (err) {
-		    return console.log(err);
+	var path_explode = name.split('/');
+	var last_value = path_explode[path_explode.length-1];
+	if(last_value.indexOf('.css') > 0){
+		fs.readFile(name, 'utf8', function (err,data) {
+			if (err)
+			    return console.log(err);
+
+			var class_to_find = 'label-blu';
+			var reg = new RegExp('\\.' + RegExp.quote(class_to_find) + '([,:\\s\\.][^\\{]*)?\\{', 'gmi');
+			var found = data.match(reg);
+			
+			if((found != '') && (found != null))
+				found = found.length;
+			else
+				found = 0;
+
+			console.log('found in ' + name + ': ' + found + ' match of ' + class_to_find);
+		});
+	}
+	else{
+		if(last_value.indexOf('.') == -1){
+			fs.readdir(name, function(err,files){
+			    if (err) 
+			    	return console.log(err);
+
+			    var c=0;
+			    files.forEach(function(file){
+			    	var path_explode = file.split('/');
+					var last_value = path_explode[path_explode.length-1];
+					if(last_value.indexOf('.css') > 0){
+				        fs.readFile(name+file,'utf-8',function(err, data){
+				            if (err) 
+				            	return console.log(err);
+
+				            var class_to_find = 'label-blu';
+							var reg = new RegExp('\\.' + RegExp.quote(class_to_find) + '([,:\\s\\.][^\\{]*)?\\{', 'gmi');
+							var found = data.match(reg);
+							
+							if((found != '') && (found != null))
+								found = found.length;
+							else
+								found = 0;
+
+							console.log('found in ' + name + file + ': ' + found + ' match of ' + class_to_find);
+				        });
+				    }
+			    });
+			});
 		}
-
-		var class_to_find = 'label-blu';
-		var reg = new RegExp('\\.' + RegExp.quote(class_to_find) + '([,:\\s\\.][^\\{]*)?\\{', 'gmi');
-		var found = data.match(reg);
-		
-		if((found != '') && (found != null))
-			found = found.length;
-		else
-			found = 0;
-
-		console.log('found in ' + name + ': ' + found + ' match of ' + class_to_find);
-	});
+	}
 };
