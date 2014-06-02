b7113840b9b9030e17ec24c354bcebb8b7f122de
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-06-02 21:59:19 +0200

    added help function

--- cli.js
+++ cli.js
@@ -3,6 +3,21 @@
 var pkg = require('./package.json');
 var find = require('./index');
 
+function help() {
+	console.log(pkg.description);
+	console.log('');
+	console.log('Usage');
+	console.log('  $ find-css-class <css-class-to-find> <folder-or-file-to-search>');
+	console.log('');
+	console.log('Example');
+	console.log('  $ find-css-class clear css/style.css');
+}
+
+if (!input || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
+	help();
+	return;
+}
+
 var find_class = process.argv[2];
 var folder = process.argv[3];
 if((find != null) && (find != '') && (find != undefined) & (folder != null) && (folder != '') && (folder != undefined)){
