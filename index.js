1c65e8d56040c7285f158d8dbabfab4020139690
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-06-08 21:42:23 +0200

    find class into html file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ RegExp.quote = function(str) {
   return (str + '').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
 };
 
+
 function getDirectoryFiles(directory, callback) {
     fs.readdir(directory, function(err, files) {
         files.forEach(function(file){
@@ -42,28 +43,68 @@ module.exports = function (css_class, name, cb) {
 		});
 	}
 	else{
-		if(last_value.indexOf('.') == -1){
-			getDirectoryFiles(name, function(file) {
-			    var path_explode = file.split('/');
-				var last_value = path_explode[path_explode.length-1];
-				if(last_value.indexOf('.css') > 0){
-				    fs.readFile(file,'utf-8',function(err, data){
-				        if (err) 
-				        	return console.log(err);
+		if(last_value.indexOf('.html') > 0){
+  			fs.readFile(name, 'utf8', function (err,data) {
+				if (err)
+				    return console.log(err);
 
-				        var class_to_find = css_class;
-						var reg = new RegExp('\\.' + RegExp.quote(class_to_find) + '([,:\\s\\.][^\\{]*)?\\{', 'gmi');
-						var found = data.match(reg);
-						
-						if((found != '') && (found != null))
-							found = found.length;
-						else
-							found = 0;
+				var class_to_find = css_class;
+				
+				var reg = new RegExp("(<\\w+?\\s+?class\\s*=\\s*['\"][^'\"]*?\\b)" + class_to_find + "\\b", "i");
 
-						cb(null, 'found in: ' + file + ': ' + found + ' match of ' + class_to_find);
-				    });
-				}
+				var found = data.match(reg);
+				
+				if((found != '') && (found != null))
+					found = found.length;
+				else
+					found = 0;
+
+				cb(null, 'found in: ' + name + ': ' + found + ' match of ' + class_to_find);
 			});
+		}
+		else{
+			if(last_value.indexOf('.') == -1){
+				getDirectoryFiles(name, function(file) {
+				    var path_explode = file.split('/');
+					var last_value = path_explode[path_explode.length-1];
+					if(last_value.indexOf('.css') > 0){
+					    fs.readFile(file,'utf-8',function(err, data){
+					        if (err) 
+					        	return console.log(err);
+
+					        var class_to_find = css_class;
+							var reg = new RegExp('\\.' + RegExp.quote(class_to_find) + '([,:\\s\\.][^\\{]*)?\\{', 'gmi');
+							var found = data.match(reg);
+							
+							if((found != '') && (found != null))
+								found = found.length;
+							else
+								found = 0;
+
+							cb(null, 'found in: ' + file + ': ' + found + ' match of ' + class_to_find);
+					    });
+					}
+					else{
+						if(last_value.indexOf('.html') > 0){
+							fs.readFile(name, 'utf8', function (err,data) {
+								if (err)
+								    return console.log(err);
+
+								var class_to_find = css_class;
+								var reg = new RegExp("(<\\w+?\\s+?class\\s*=\\s*['\"][^'\"]*?\\b)" + class_to_find + "\\b", "i");
+								var found = data.match(reg);
+								
+								if((found != '') && (found != null))
+									found = found.length;
+								else
+									found = 0;
+
+								cb(null, 'found in: ' + name + ': ' + found + ' match of ' + class_to_find);
+							});
+						}
+					}
+				});
+			}
 		}
 	}
 };
